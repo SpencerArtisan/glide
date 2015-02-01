b73e2bfca17e6b164a0cfc63b6eb1771dc517fb5
Spencer Ward <SpencerArtisan>
2015-02-01 22:52:21 +0000

    Rework text area colouring

--- core/src/com/mygdx/game/CodingScreen.java
+++ core/src/com/mygdx/game/CodingScreen.java
@@ -17,28 +17,23 @@ public class CodingScreen extends ScreenAdapter {
 	private Stage stage;
 	private BitmapFont font;
 	private ScrollPane scrollPane;
-//	private TextButton.TextButtonStyle tbStyle;
-//	private TextFieldStyle textFieldStyle;
 	
 	public CodingScreen(Viewport viewport, ResourceManager resourceManager) {
 		this.stage = new Stage(viewport);
 
-//		font = new BitmapFont(Gdx.files.internal("fonts/white-rabbit.fnt"));
-		
 //		ImageArea imageArea = new ImageArea();
 //		SplitPaneStyle spStyle = new SplitPane.SplitPaneStyle();
 //		spStyle.handle = new TextureRegionDrawable(new TextureRegion(
 //				new Texture(Gdx.files.internal("images/divider.png"))));
 //		SplitPane sp = new SplitPane(textArea, imageArea, false, spStyle);
 
-		
 		Skin skin = resourceManager.getSkin();
 		TextArea textArea = createTextArea(viewport, skin);
 
 	    scrollPane = new ScrollPane(textArea, skin);
 		scrollPane.setWidth(viewport.getWorldWidth());
 		scrollPane.setHeight(viewport.getWorldHeight());
-//		scrollPane.setFadeScrollBars(false);
+		scrollPane.setFadeScrollBars(false);
 		stage.addActor(scrollPane);
 		stage.setKeyboardFocus(textArea);
 
@@ -49,22 +44,10 @@ public class CodingScreen extends ScreenAdapter {
 		TextArea textArea = new TextAreaExt(Code.groovyTemplate(), skin);
 		textArea.setPrefRows(44);
 		textArea.setCursorPosition(textArea.getText().length());
-		textArea.getStyle().font.setMarkupEnabled(true);
+//		textArea.getStyle().font.setMarkupEnabled(true);
 		return textArea;
 	}
 	
-//	private void createStyles() {
-//		textFieldStyle = new TextField.TextFieldStyle();
-//		textFieldStyle.font = font;
-//		font.setMarkupEnabled(true);
-//		textFieldStyle.fontColor = Color.RED;
-//
-//		Texture tfSelection = new Texture(Gdx.files.internal("images/tfSelection.png"));
-//        Texture tfCursor = new Texture(Gdx.files.internal("images/cursor.png"));
-//        textFieldStyle.selection = new TextureRegionDrawable(new TextureRegion(tfSelection));
-//        textFieldStyle.cursor = new TextureRegionDrawable(new TextureRegion(tfCursor)).tint(Color.GREEN);
-//	}
-
     @Override
     public void render(float delta) { 
         Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -79,6 +62,4 @@ public class CodingScreen extends ScreenAdapter {
 		super.resize(width, height);
 		scrollPane.setSize(width, height);
 	}  	  
-    
-    
 }
--- core/src/com/mygdx/game/textarea/TextAreaExt.java
+++ core/src/com/mygdx/game/textarea/TextAreaExt.java
@@ -1,7 +1,5 @@
 package com.mygdx.game.textarea;
 
-import javafx.scene.input.KeyCode;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -9,9 +7,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextArea.TextAreaListener;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.IntArray;
 
 public class TextAreaExt extends com.badlogic.gdx.scenes.scene2d.ui.TextArea {
 	public TextAreaExt(String text, Skin skin, String styleName) {
@@ -25,6 +21,34 @@ public class TextAreaExt extends com.badlogic.gdx.scenes.scene2d.ui.TextArea {
 	public TextAreaExt(String text, TextFieldStyle style) {
 		super(text, style);
 	}
+	
+	@Override
+	protected void drawText (Batch batch, BitmapFont font, float x, float y) {
+		String colouredText = "[GRAY]" + text;
+		font.setMarkupEnabled(true);
+		
+		float offsetY = 0;
+		IntArray linesBreak = new IntArray();
+		int newLineIndex = 0;
+		while(newLineIndex < colouredText.length()) {
+			linesBreak.add(newLineIndex);
+			newLineIndex = colouredText.indexOf('\n', newLineIndex);
+			if (newLineIndex == -1) {
+				linesBreak.add(colouredText.length());
+				break;
+			}
+			linesBreak.add(newLineIndex);
+			newLineIndex++;
+		}
+				
+		for (int i = getFirstLineShowing() * 2; i < (getFirstLineShowing() + getLinesShowing()) * 2 && i < linesBreak.size; i += 2) {
+			font.draw(batch, colouredText, x, y + offsetY, linesBreak.items[i], linesBreak.items[i + 1]);
+			offsetY -= font.getLineHeight();
+		}
+
+		font.setMarkupEnabled(false);
+	}
+
 
 	@Override
 	protected InputListener createInputListener () {
@@ -32,7 +56,6 @@ public class TextAreaExt extends com.badlogic.gdx.scenes.scene2d.ui.TextArea {
 	}
 
 	public class TextAreaExtListener extends TextAreaListener {
-
 		@Override
 		public boolean touchDown(InputEvent event, float x, float y,
 				int pointer, int button) {
@@ -43,14 +66,12 @@ public class TextAreaExt extends com.badlogic.gdx.scenes.scene2d.ui.TextArea {
 
 		@Override
 		public boolean keyTyped(InputEvent event, char character) {
+			// Prevent command c/v defect
 			boolean commandPressed = Gdx.input.isKeyPressed(Keys.SYM);
 			if (commandPressed) {
 				return true;
 			}
 			return super.keyTyped(event, character);
-
 		}
-		
 	}
-
 }
