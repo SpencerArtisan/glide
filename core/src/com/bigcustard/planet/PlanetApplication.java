e1625e840f0a4ba588641a68cd16e625225a3ce5
Spencer Ward <spencerward@Spencers-MBP.lan>
2015-03-29 22:16:39 +0100

    Close run screen

--- core/src/com/bigcustard/planet/PlanetApplication.java
+++ core/src/com/bigcustard/planet/PlanetApplication.java
@@ -1,19 +1,29 @@
 package com.bigcustard.planet;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.bigcustard.blurp.core.BlurpRuntime;
 import com.bigcustard.blurp.model.BlurpMain;
 import com.bigcustard.blurp.model.ImageSprite;
+import com.bigcustard.blurp.ui.BlurpScreen;
+import com.bigcustard.blurp.ui.RenderListener;
 import com.bigcustard.planet.code.Game;
 import com.bigcustard.planet.screen.CodingScreen;
 import com.bigcustard.planet.screen.GameLibraryDialog;
 import com.bigcustard.planet.screen.ResourceManager;
 import com.bigcustard.planet.screen.WelcomeScreen;
+import com.bigcustard.scene2dplus.XY;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 
@@ -47,7 +57,30 @@ public class PlanetApplication extends com.badlogic.gdx.Game {
             }
         };
         runtime.start(script);
-        setScreen(runtime.getScreen());
+        BlurpScreen blurpScreen = runtime.getScreen();
+        setScreen(blurpScreen);
+        blurpScreen.setRenderListener((batch, delta, eventType) -> {
+            if (eventType == RenderListener.EventType.PreRender) {
+                int crossX = viewport.getScreenWidth() - 40;
+                int crossY = viewport.getScreenHeight() - 40;
+
+                Drawable closeIcon = resourceManager.getSkin().getDrawable("close");
+                closeIcon.draw(batch, crossX, crossY, 32, 32);
+
+                if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && clickedOnClose(crossX, crossY)) {
+                    runtime.end();
+                    showCodingScreen(() -> game);
+                }
+            }
+        });
+    }
+
+    private boolean clickedOnClose(int crossX, int crossY) {
+        int x = Gdx.input.getX();
+        int y = Gdx.input.getY();
+        Vector3 world = viewport.getCamera().unproject(new Vector3(x, y, 0));
+        return world.x > crossX && world.x < crossX + 32 &&
+                world.y > crossY && world.y < crossY + 32;
     }
 
     private void configureGameLibraryButton(WelcomeScreen welcomeScreen) {
