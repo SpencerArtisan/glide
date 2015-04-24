f97bd3a33606fcef36f3ad5ea1ffd5548a7bb3db
Spencer <spencerartisan@gmail.com>
2015-04-24 19:18:15 +0100

    Add moving world example

---
+++ android/assets/games/Multiple Worlds/code.groovy
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////// 
+//         Welcome to Planet Burpl!             // 
+//                                              // 
+// Use Arrays to show lots of identical sprites // 
+//     Use For to loop through the sprites      //
+////////////////////////////////////////////////// 
+
+// Create 4 worlds and spread them out horizontally
+worlds = []
+for (i in 1..4) {
+    world = blurp.imageSprite("world.png")
+    world.x = i * 350
+    world.y = 900
+    worlds.add world
+}
+
+// Now move the worlds
+while (true) {
+    for (world in worlds) {
+        world.y -= 10
+    }
+    blurp.blurpify()
+}
