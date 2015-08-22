2bdcaa248634b6c7803fa408ba45a5f8e37bb3ba
Spencer <spencerartisan@gmail.com>
2015-08-22 11:40:19 +0100

    Dummy clipboard and file buttons

--- core/src/com/bigcustard/scene2dplus/resource/ResourceArea.java
+++ core/src/com/bigcustard/scene2dplus/resource/ResourceArea.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Disposable;
 import com.bigcustard.scene2dplus.command.CommandHistory;
 import com.google.common.collect.Lists;
@@ -17,29 +18,45 @@ import java.util.List;
 public class ResourceArea<TModel> extends ScrollPane implements Disposable {
     private Skin skin;
     private final ResourceSet<TModel> resources;
+    private TextButton clipboardButton;
+    private TextButton fileButton;
 
     public ResourceArea(Skin skin, ResourceSet<TModel> resources) {
         super(new Table(), skin);
         this.skin = skin;
         this.resources = resources;
         this.setScrollingDisabled(true, false);
+        createClipboardButton(skin);
+        createFileButton(skin);
         layoutControls();
         pack();
         resources.resources().watch((list) -> layoutControls());
     }
 
+    private void createClipboardButton(Skin skin) {
+        clipboardButton = new TextButton("Add from clipboard", skin);
+    }
+
+    private void createFileButton(Skin skin) {
+        fileButton = new TextButton("Add from file", skin);
+    }
+
     private void layoutControls() {
-        Table layoutTable = (Table) getWidget();
-        layoutTable.defaults().pad(12);
-        layoutTable.background(skin.getDrawable("solarizedNew"));
-        layoutTable.clearChildren();
-        layoutTable.top();
+        Table table = (Table) getWidget();
+        table.defaults().pad(12).padBottom(0);
+        table.background(skin.getDrawable("solarizedNew"));
+        table.clearChildren();
+        table.top();
+        table.add(clipboardButton).fillX();
+        table.row();
+        table.add(fileButton).fillX();
+        table.row();
         for (Resource resource : resources.resources()) {
             Actor editor = resource.editor();
-            layoutTable.add(editor);
-            layoutTable.row();
+            table.add(editor);
+            table.row();
         }
-        layoutTable.pack();
+        table.pack();
     }
 
     @Override
