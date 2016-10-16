5f46e3577e8be18821d45ffb39b6e6d1ed7b1151
Spencer <spencerartisan@gmail.com>
2016-10-16 22:46:37 +0100

    Examples from files

---
+++ android/assets/examples/Set background colour.py
@@ -0,0 +1,1 @@
+screen.backgroundColour = Red
--- core/src/com/bigcustard/glide/help/Help.java
+++ core/src/com/bigcustard/glide/help/Help.java
@@ -1,13 +1,34 @@
 package com.bigcustard.glide.help;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.bigcustard.glide.code.Game;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Help {
+    private static String EXAMPLES_FOLDER = "examples";
+
     public List<HelpTopic> topics() {
-        HelpTopic topic1 = new HelpTopic("Show sprite", "some code");
-        HelpTopic topic2 = new HelpTopic("Position sprite", "some more code");
-        return ImmutableList.of(topic1, topic2);
+        List<FileHandle> examples = Arrays.asList(allExamples(examplesFolder()));
+        return examples.stream().map(this::toTopic).collect(Collectors.toList());
     }
+
+    private HelpTopic toTopic(FileHandle helpFile) {
+        return new HelpTopic(helpFile.name().substring(0, helpFile.name().indexOf(".")), helpFile.readString());
+    }
+
+    private FileHandle[] allExamples(FileHandle parentFolder) {
+        return parentFolder.list(file -> !file.isDirectory() && !file.getName().startsWith("."));
+    }
+
+    protected FileHandle examplesFolder() {
+        return Gdx.files.internal(EXAMPLES_FOLDER);
+    }
+
 }
